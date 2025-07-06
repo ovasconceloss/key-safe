9f6c3e9c82aa9a6bbe831b99ff56ae44bd1420e6
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-06 14:59:23 +0100

    feat(config): start the back-end project with dotnet cli

---
+++ back-end/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
