4c9d7963a8370131a7c4b925fb3251e69a2b6587
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-07-06 21:00:13 +0100

    feat(db): create the connection string dynamically in program.cs

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -1,16 +1,24 @@
 using DotNetEnv;
+using back_end.Data;
+using Microsoft.EntityFrameworkCore;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
-var passwordFromEnvironment = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection__Password");
+
+var dbHost = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection__Host");
+var dbDatabase = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection__Database");
+var dbUsername = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection__Username");
+var dbPassword = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection__Password");
+
+var connectionString = $"Host={dbHost}; Database={dbDatabase}; Username={dbUsername}; SSL Mode=VerifyFull; Channel Binding=Require; Password={dbPassword};";
 
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json")
     .AddEnvironmentVariables();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
